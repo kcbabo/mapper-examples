60df6a7840e47edc18e703f360831f1c158ec2a5
Keith Babo <kbabo@redhat.com>
2014-08-29 15:15:41 -0400

    adding xsd import example; error on import

--- src/main/java/org/jboss/mapper/example/ExampleDriver.java
+++ src/main/java/org/jboss/mapper/example/ExampleDriver.java
@@ -12,15 +12,23 @@ public class ExampleDriver {
     private static final String TEST_CONFIGURATION_PATH = "modeler-config.json";
     private static final String TEST_REPOSITORY_STORE_PARENT_PATH = "target/test-repository";
     private static final String METAMODEL_REPOSITORY_URL_PATH = "file:metamodel-repository";
-    private static final String METAMODEL_ID = "org.modeshape.modeler.java.JavaFile";
+    
+
+    private static final String JAVA_METAMODEL_CATEGORY = "java";
+    private static final String JAVA_METAMODEL_ID = "org.modeshape.modeler.java.JavaFile";
     private static final String JAVA_SOURCE_MODEL_PATH = "src/main/java/org/jboss/mapper/example/order/abc/ABCOrder.java";
+
+    private static final String XSD_METAMODEL_CATEGORY = "xsd";
+    private static final String XSD_METAMODEL_ID = "org.modeshape.modeler.xsd.Xsd";
+    private static final String XSD_MODEL_PATH = "src/main/resources/abc-order.xsd";
     
     private static Modeler modeler;
 
     public static void main(String[] args) {
         
         try {
-            importJavaSourceIntoModel();
+            //importJavaSourceIntoModel();
+            importXSDIntoModel();
         } catch (Throwable t) {
             t.printStackTrace();
         } finally {
@@ -29,17 +37,25 @@ public class ExampleDriver {
     }
     
     public static void importJavaSourceIntoModel() throws Exception {
-        initModeler("java");
-        Metamodel metamodel = modeler.metamodelManager().metamodel(METAMODEL_ID);
+        initModeler(JAVA_METAMODEL_CATEGORY);
+        Metamodel metamodel = modeler.metamodelManager().metamodel(JAVA_METAMODEL_ID);
         Model model = modeler.importModel(new File(JAVA_SOURCE_MODEL_PATH), null, metamodel);
         model.print();
             
     }
     
+    public static void importXSDIntoModel() throws Exception {
+        initModeler(XSD_METAMODEL_CATEGORY);
+        Metamodel metamodel = modeler.metamodelManager().metamodel(XSD_METAMODEL_ID);
+        Model model = modeler.importModel(new File(XSD_MODEL_PATH), null, metamodel);
+        model.print();
+    }
+    
     private static void initModeler(String category) throws Exception {
         modeler = Modeler.Factory.instance( TEST_REPOSITORY_STORE_PARENT_PATH, TEST_CONFIGURATION_PATH );
-        for ( final URL url : modeler.metamodelManager().metamodelRepositories())
+        for ( final URL url : modeler.metamodelManager().metamodelRepositories()) {
             modeler.metamodelManager().unregisterMetamodelRepository(url);
+        }
         modeler.metamodelManager().registerMetamodelRepository(new URL(METAMODEL_REPOSITORY_URL_PATH));
         modeler.metamodelManager().install(category);
     }
